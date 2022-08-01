14369c79469f3eaaf25a758df6515f496afb7b7b
Daniel Au <daniel.au05@gmail.com>
2022-08-01 13:17:14 -0700

    finished!

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// TODO
+var GroceryList = (props) => (
+  <div> DANIEL's GROCERY LIST
+    <ul>
+      {props.groceryArray.map(item =>
+        <GroceryListItem item={item} />
+      )}
+    </ul>
+  </div>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      completed: false,
+      hover: false
+    };
+  }
+
+  onClick() {
+    this.setState({
+      completed: !this.state.completed
+    });
+  }
+
+  onMouseEnter() {
+    this.setState({
+      hover: true
+    });
+  }
+
+  onMouseLeave() {
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.completed ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onClick={this.onClick.bind(this)} onMouseEnter={this.onMouseEnter.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)}>
+      {this.props.item}
+      </li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList groceryArray={['Cucumbers', 'Kale', 'Potatoes', 'Ramen', 'Tomatoes']}/>, document.getElementById('app'));
